ab53eb69282208f24181f1e408d9a44ee67b5331
elliot <dhaka.sonu.monu@gmail.com>
2021-11-02 11:25:42 +0530

    add |\-/ animation bar at the end

--- 57-progress-bar.py
+++ 57-progress-bar.py
@@ -8,27 +8,52 @@ def main():
     print('Downloading file ...')
     curr_download_bytes=0
     total_download_bytes=4096
+    tic=time.time()
+
     while curr_download_bytes<total_download_bytes:
 
         # download some bytes
-        curr_download_bytes+=random.randint(0,500)
+        curr_download_bytes+=random.randint(0,1000)
 
         progress_string=progress_bar(curr_download_bytes,total_download_bytes)
         
         # since default end for print is new-line we specify end as ''
+
+        print('\b'*(len(progress_string)+1),end='',flush=True)
+        
         print(progress_string,end='',flush=True)
-        time.sleep(0.5)
-        print('\b'*len(progress_string),end='',flush=True)
+        for i in range(5):
+            print('|',end='',flush=True)
+            time.sleep(0.1)
+            print('\b',end='',flush=True)
+            print('\\',end='',flush=True)
+            time.sleep(0.1)
+            print('\b',end='',flush=True)
+            print('-',end='',flush=True)
+            time.sleep(0.1)
+            print('\b',end='',flush=True)
+            print('/',end='',flush=True)
+            time.sleep(0.1)
+            print('\b',end='',flush=True)
+    
+        # time.sleep(0.2)
+        # print('\b'*(len(progress_string)+1),end='',flush=True) # this is an important step
+
+    toc=time.time()
+    # print('\n') 
+    print('\b'*(len(progress_string)+1),end='\n',flush=True)
+
+    print(f'Downloaded in {round(toc-tic)} seconds')
 
 
     
 # returns a string 
-def progress_bar(progress,total,bar_len=30):
+def progress_bar(progress,total,bar_len=40):
     progress_string=""
     progress_string+="["
 
     # main bar progress
-    # to make sure that progress is between 0 and 1
+    # to make sure that progress is between 0 and total
     if progress>total:
         progress=total
     if progress<0:
@@ -45,12 +70,12 @@ def progress_bar(progress,total,bar_len=30):
     progress_string+="] "
 
     # add download data
-    download_percent=round(100*(progress/total),1)
+    download_percent=round(100*(progress/total))
 
     progress_string+=str(download_percent) + "%"
 
     progress_string+=" "
-    progress_string+=str(progress)+"/"+str(total)
+    progress_string+=str(progress)+"/"+str(total)+ "  "
 
     return progress_string
 
