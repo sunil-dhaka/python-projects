14788816ff3e5f73d0d9ba10032c041ba1fa5560
elliot <dhaka.sonu.monu@gmail.com>
2021-11-04 09:04:16 +0530

    modify caesar cipher to encrypt of decrypt for any general symboles

--- 6-caesar1.py
+++ 6-caesar1.py
@@ -3,6 +3,9 @@ try:
 except Exception as error:
     print(f'Error >> {error}')
 
+# add symbols that you want to encrypt or decrypt
+SYMBOLES='ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*~1234567890'
+
 def main():
     try:
         print('''
@@ -10,7 +13,6 @@ The Caesar cipher encrypts letters by shifting them over by a
 key number. For example, a key of 2 means the letter A is
 encrypted into C, the letter B encrypted into D, and so on.
         ''')
-
         # take correct inputs
     
         while True:
@@ -42,6 +44,24 @@ encrypted into C, the letter B encrypted into D, and so on.
         sys.exit()
 
 def encrypt_decrypt(message,key):
+    symbol_list=[c for c in SYMBOLES]
+    encrpted_text=[]
+    for c in message:
+        if c in symbol_list:
+            pos=symbol_list.index(c)
+            pos=pos+key
+            if pos>len(symbol_list): # might happen in encryption
+                pos=pos-len(symbol_list)
+            if pos<0:# might happen in decryption
+                pos=pos-len(symbol_list)
+            encrpted_text.append(symbol_list[pos])
+        else:
+            encrpted_text.append(c)
+    return ''.join(encrpted_text)
+
+'''
+# this func uses ord() and chr() to convert into numeric and char
+def encrypt_decrypt(message,key):
     text=[c for c in message]
     encrpted_text=[]
     start_ord=ord('A')
@@ -54,6 +74,7 @@ def encrypt_decrypt(message,key):
             encrpted_text.append(char)
         
     return ''.join(encrpted_text)
+'''
 
 if __name__=='__main__':
     main()
