9f741aafc1d7c8f0d8fa1dce845726ac7a108ceb
elliot <dhaka.sonu.monu@gmail.com>
2021-11-04 09:27:32 +0530

    add simple caesar hacker
    - when you do not know the encryption key just simple can try out all different combinations and see which one works
    - that is what is hacking here; Hack the key.

---
+++ 7-caeser2.py
@@ -0,0 +1,34 @@
+# add symbols that you want to encrypt or decrypt
+SYMBOLES='ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*~1234567890'
+
+def main():
+    # try:
+    print('Enter the encrypted Caesar cipher message to hack.')
+    message=input('> ')
+
+    for i in range(len(SYMBOLES)):
+        print(f'Key #{i}({SYMBOLES[i]}):',end=' ')
+        print(caeser_hack(message,-i)) # all decryption 
+        
+    '''except Exception as e:
+        print(f'Error: {e}')
+        exit()'''
+
+def caeser_hack(message,key):
+    symbol_list=[c for c in SYMBOLES]
+    encrpted_text=[]
+    for c in message:
+        if c in symbol_list and c.isupper():
+            pos=symbol_list.index(c)
+            pos=pos+key
+            if pos>len(symbol_list): # might happen in encryption
+                pos=pos-len(symbol_list)
+            if pos<0:# might happen in decryption
+                pos=pos+len(symbol_list)
+            encrpted_text.append(symbol_list[pos])
+        else:
+            encrpted_text.append(c)
+    return ''.join(encrpted_text)
+
+if __name__=='__main__':
+    main()
